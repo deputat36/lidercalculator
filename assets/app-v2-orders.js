ce59145f0e23bb42504c6993b6cfc80ef75260d0
deputat36 <a.v.kovtun@borisoglebsk.etagi.com>
2026-05-23 09:56:30 +0300

    Load calculation summary module in CRM v2

--- assets/app-v2-orders.js
+++ assets/app-v2-orders.js
@@ -13,4 +13,5 @@
   loadScript('assets/app-v2-orders-pro.js');
   loadScript('assets/app-v2-catalog'+'-calc.js');
   loadScript('assets/app-v2-calc'+'-editor.js');
+  loadScript('assets/app-v2-calc'+'-summary.js');
 })();
