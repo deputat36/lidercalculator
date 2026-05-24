2af3a872609a333b0aec5d702691523fbd725280
deputat36 <a.v.kovtun@borisoglebsk.etagi.com>
2026-05-24 20:30:41 +0300

    Обновить версию после добавления истории цен

--- assets/app-v2-orders.js
+++ assets/app-v2-orders.js
@@ -7,7 +7,7 @@
       document.body.appendChild(s);
     }
   }
-  var v='?v=20260524-6';
+  var v='?v=20260524-7';
   loadScript('assets/app-v2-speed-core.js'+v);
   loadScript('assets/app-v2-dashboard.js'+v);
   loadScript('assets/app-v2-orders-pro.js'+v);
