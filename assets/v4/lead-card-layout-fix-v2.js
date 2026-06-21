f4accad7899d400af2541824d6fb0f1b26bdc527
deputat36 <a.v.kovtun@borisoglebsk.etagi.com>
2026-06-21 16:43:41 +0300

    Load clearer lead card UX helper

--- assets/v4/lead-card-layout-fix-v2.js
+++ assets/v4/lead-card-layout-fix-v2.js
@@ -8,6 +8,7 @@ import './lead-action-autolog-v1.js?v=20260618-1';
 import './orders-fast-loader-v1.js?v=20260619-1';
 import './offers-fast-loader-v1.js?v=20260619-1';
 import './control-sections-lazy-loader-v1.js?v=20260619-4';
+import './lead-card-ux-v1.js?v=20260621-1';
 
 let currentLeadId = null;
 
