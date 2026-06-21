cda5e4b8b59f1116e3c692bcef16218b0db3cad3
deputat36 <a.v.kovtun@borisoglebsk.etagi.com>
2026-06-21 17:15:28 +0300

    Load management dashboard in CRM v4

--- assets/v4/offer-print.js
+++ assets/v4/offer-print.js
@@ -1,3 +1,4 @@
 import './offer-print-brand-v4.js?v=20260621-logo-2';
 import './finance-control-v2.js?v=20260621-1';
+import './management-dashboard-v2.js?v=20260621-1';
 import './site-cache-note-v1.js?v=20260621-1';
